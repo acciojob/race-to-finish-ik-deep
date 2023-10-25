351daf909333f228bb8a7553a51d701e1047d45a
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-25 17:11:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.promises = [];
+// Array of five promises with random delays between 1 and 5 seconds
+const promises = Array.from({ length: 5 }, (_, index) => {
+    const delay = Math.floor(Math.random() * 5000) + 1000; // Random delay between 1 and 5 seconds
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Promise ${index + 1} resolved after ${delay / 1000} seconds`);
+        }, delay);
+    });
+});
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+    .then((result) => {
+        const outputDiv = document.getElementById("output");
+        outputDiv.textContent = result;
+    })
+    .catch((error) => {
+        console.error("All promises were rejected:", error);
+    });
